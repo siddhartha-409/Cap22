205238ccfb1cba3c7db52ba04623905daa07498a
siddhartha-409 <sidvar19@gmail.com>
2022-02-03 09:25:44 +0530

    my day4 java progs

--- Day 4/PercentOfInterest.java
+++ Day 4/PercentOfInterest.java
@@ -14,22 +14,22 @@ public class PercentOfInterest {
 		
 		
 		
-		if((g=="Female" && a>=1) && a<=58) {
+		if(g.equals("Female") && a>=1 && a<=58) {
 			
 			System.out.println("Interest = 8.2%");
 		}
 		
-		else if(g=="Female" && a>=59 && a<=120) {
+		else if(g.equals("Female") && a>=59 && a<=120) {
 			
 			System.out.println("Interest = 7.6%");
 		}
 		
-		else if(g=="Male" && a>=1 && a<=60) {
+		else if(g.equals("Male") && a>=1 && a<=60) {
 	
 			System.out.println("Interest = 8.3%");
 		}
 		
-		else if(g=="Male" && a>=61 && a<=120) {
+		else if(g.equals("Male") && a>=61 && a<=120) {
 			
 			System.out.println("Interest = 8.3%");
 		}
